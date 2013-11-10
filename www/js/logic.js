c813146b8bcfda823090581fc0fec8eaa5c9c473
Daniel <adgranados@gmail.com>
2013-11-10 12:08:59 -0500

    esta mal

--- www/js/logic.js
+++ www/js/logic.js
@@ -1,8 +1,33 @@
 $(function(){
 
+
+
      $(document).on("pageinit","#question",function(event){
+
+
+     	function playAudio(url) {
+		    // Play the audio file at url
+		    var my_media = new Media(url,
+		        // success callback
+		        function() {
+		            console.log("playAudio():Audio Success");
+		        },
+		        // error callback
+		        function(err) {
+		            console.log("playAudio():Audio Error: "+err);
+		    });
+
+		    // Play audio
+		    my_media.play();
+
+		    // Pause after 10 seconds
+		    /*setTimeout(function() {
+		        media.pause();
+		    }, 10000);*/        
+		}
      	$("#play").click(function(){
-     		$("#bien")[0].play();
+     		//$("#bien")[0].play();
+     		playAudio("sounds/bien.mp3")     		
      	})
 
 		PCSV = {
